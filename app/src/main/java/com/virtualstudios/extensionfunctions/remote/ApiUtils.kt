137144f56345a79eea8e0c6dd8203189d20d0799
Virender Srxn <virendersran01@gmail.com>
2023-06-03 14:18:44 +0530

    ext.

--- app/src/main/java/com/virtualstudios/extensionfunctions/remote/ApiUtils.kt
+++ app/src/main/java/com/virtualstudios/extensionfunctions/remote/ApiUtils.kt
@@ -46,6 +46,13 @@ sealed class ApiCallResult<out T>{
     }
 
 }
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+}
+
 @Keep
 sealed class UiState<out T> {
     class Loading<T> : UiState<T>()
