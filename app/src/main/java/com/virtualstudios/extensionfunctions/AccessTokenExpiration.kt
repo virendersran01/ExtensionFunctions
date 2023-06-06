20ee956b3e81846751f1cd35f6da979276169fd1
Virender Srxn <virendersran01@gmail.com>
2023-06-06 21:08:02 +0530

    AccessTokenExpiration.

--- app/src/main/java/com/virtualstudios/extensionfunctions/AccessTokenExpiration.kt
+++ app/src/main/java/com/virtualstudios/extensionfunctions/AccessTokenExpiration.kt
@@ -1,12 +1,22 @@
 package com.virtualstudios.extensionfunctions
 
+import android.content.Context
 import com.google.gson.annotations.SerializedName
+import dagger.Module
+import dagger.Provides
 import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.Response
+import okhttp3.Route
+import retrofit2.Converter
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 interface ApiService {
     @POST("/api/v1/users/refresh-token/")
@@ -76,6 +86,7 @@ class SessionManager {
         accessTokenExpirationTime = System.currentTimeMillis() + expiresIn * 1000 // Convert expiresIn to milliseconds
     }
 
+
     // Other session management methods
 }
 
@@ -89,3 +100,185 @@ val retrofit = Retrofit.Builder()
     .client(okHttpClient)
     // Other Retrofit configuration
     .build()
+
+
+/**
+ * Interceptor to add auth token to requests
+ */
+class AuthInterceptor2(context: Context) : Interceptor {
+    private val sessionManager = SessionManager(context)
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestBuilder = chain.request().newBuilder()
+        sessionManager.fetchAuthToken()?.let {
+            requestBuilder.addHeader(
+                "Authorization", "Bearer $it"
+            )
+        }
+        return chain.proceed(requestBuilder.build())
+    }
+}
+
+//-------------------------------------------------------------------------------------------
+
+/**
+ * Authenticator to get the RefreshToken if current token is expired
+ */
+class AuthenticateApi(context: Context) : Authenticator {
+    private val sessionManager = SessionManager2(context)
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        val apiClient = Retrofit.Builder().baseUrl(ApiUrl.BASE_URL) // api base url
+            .addConverterFactory(GsonConverterFactory.create()).build()
+            .create(ApiService::class.java)
+        return runBlocking {
+            // call login api again for getting accessToken in runBlocking so that it will wait until api response come
+            val apiResponse = apiClient.login(LoginBody("piash599@gmail.com", "p1234", "user"))
+            if (apiResponse.isSuccess) {
+                val accessToken = apiResponse.data?.accessToken
+                accessToken?.let {
+                    sessionManager.saveAuthToken(accessToken)
+                }
+                response.request.newBuilder().addHeader(
+                    "Authorization", "Bearer $accessToken"
+                ).build()
+            } else {
+                null
+            }
+        }
+    }
+}
+
+interface ApiService {
+    @POST(ApiUrl.USER_LOGIN)
+    suspend fun login(@Body loginBody: LoginBody): BaseModel<LoginResponse>
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    /**
+     * Provides BaseUrl as string
+     */
+    @Singleton
+    @Provides
+    fun provideBaseURL(): String {
+        return ApiUrl.BASE_URL
+    }
+
+    /**
+     * Provides LoggingInterceptor for api information
+     */
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    /**
+     * Provides Auth interceptor for access token
+     */
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(@ApplicationContext context: Context): AuthInterceptor {
+        return AuthInterceptor(context)
+    }
+
+    /**
+     * Provides AuthenticateApi to check api is authenticate or not
+     */
+    @Singleton
+    @Provides
+    fun provideAuthenticateApi(@ApplicationContext context: Context): AuthenticateApi {
+        return AuthenticateApi(context)
+    }
+
+    /**
+     * Provides custom OkkHttp
+     */
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor,
+        authenticateApi: AuthenticateApi
+    ): OkHttpClient {
+        val okHttpClient = OkHttpClient().newBuilder()
+
+        okHttpClient.callTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.connectTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.readTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.writeTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.addInterceptor(loggingInterceptor)
+        okHttpClient.addInterceptor(authInterceptor)
+        okHttpClient.authenticator(authenticateApi)
+        okHttpClient.build()
+        return okHttpClient.build()
+    }
+    /**
+     * Provides converter factory for retrofit
+     */
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): Converter.Factory {
+        return GsonConverterFactory.create()
+    }
+
+    /**
+     * Provides ApiServices client for Retrofit
+     */
+    @Singleton
+    @Provides
+    fun provideRetrofitClient(
+        baseUrl: String, okHttpClient: OkHttpClient, converterFactory: Converter.Factory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+    /**
+     * Provides Api Service using retrofit
+     */
+    @Singleton
+    @Provides
+    fun provideRestApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
+
+/**
+ * Session manager to save and fetch data from SharedPreferences
+ */
+class SessionManager2 (context: Context) {
+    private var prefs: SharedPreferences = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
+
+    companion object {
+        const val USER_TOKEN = "user_token"
+    }
+
+    /**
+     * Function to save auth token
+     */
+    fun saveAuthToken(token: String) {
+        val editor = prefs.edit()
+        editor.putString(USER_TOKEN, token)
+        editor.apply()
+    }
+
+    /**
+     * Function to fetch auth token
+     */
+    fun fetchAuthToken(): String? {
+        return prefs.getString(USER_TOKEN, null)
+    }
+
+    /**
+     * Function to fetch auth token
+     */
+    fun clearAuthToken() {
+        prefs.edit().clear().commit()
+    }
+}
--- app/src/main/java/com/virtualstudios/extensionfunctions/remote/ApiUtils.kt
+++ app/src/main/java/com/virtualstudios/extensionfunctions/remote/ApiUtils.kt
@@ -143,3 +143,19 @@ internal fun  singleSourceOfTruth (
         }
     }
 }
+
+sealed class NetworkResponse<out T> {
+    data class Loading(
+        val isPaginating: Boolean = false,
+    ): NetworkResponse<Nothing>()
+
+    data class Success<out T>(
+        val data: T,
+        val isPaginationData: Boolean = false,
+    ): NetworkResponse<T>()
+
+    data class Failure(
+        val errorMessage: String,
+        val isPaginationError: Boolean = false,
+    ): NetworkResponse<Nothing>()
+}
