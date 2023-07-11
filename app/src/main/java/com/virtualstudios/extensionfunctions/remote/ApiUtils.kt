615b1499a531a18f85b37e77e8f4c01709bc2334
Virender Srxn <virendersran01@gmail.com>
2023-07-11 21:49:14 +0530

    added simple state data class.

--- app/src/main/java/com/virtualstudios/extensionfunctions/remote/ApiUtils.kt
+++ app/src/main/java/com/virtualstudios/extensionfunctions/remote/ApiUtils.kt
@@ -64,6 +64,25 @@ sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
 }
 
+data class State<out T>(
+    val status: Status,
+    val data: T?,
+    val message: String?
+) {
+
+    companion object {
+
+        fun <T> success(msg: String?, data: T?): State<T> = State(Status.SUCCESS, data, msg)
+        fun <T> loading(data: T?): State<T> = State(Status.LOADING, data, null)
+        fun <T> error(msg: String, data: T?): State<T> = State(Status.ERROR, data, msg)
+    }
+}
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
 @Keep
 sealed class UiState<out T> {
     object Init : UiState<Nothing>()
