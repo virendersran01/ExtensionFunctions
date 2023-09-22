5e4e0b4f248fe5c1435b619a80a2304949eb5e25
Virender Srxn <virendersran01@gmail.com>
2023-09-22 14:04:00 +0530

    ext utils.

--- app/src/main/java/com/virtualstudios/extensionfunctions/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/virtualstudios/extensionfunctions/utils/ExtensionFunctions.kt
@@ -2235,3 +2235,9 @@ inline fun <T> List<T>.forEachStep(step: Int = 1, action: (T) -> Unit) {
     }
 }
 
+fun <T1, T2> ifNotNull(value1: T1?, value2: T2?, bothNotNull: (T1, T2) -> (Unit)) {
+    if (value1 != null && value2 != null) {
+        bothNotNull(value1, value2)
+    }
+}
+
